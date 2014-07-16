d0c9fbd1367189aecd1b8a16c084ee24c7c45b1d
Will Schenk <wschenk@gmail.com>
2014-07-16 10:12:33 -0400

    Fixes #15

--- lib/generators/happy_seed/omniauth/omniauth_generator.rb
+++ lib/generators/happy_seed/omniauth/omniauth_generator.rb
@@ -27,6 +27,7 @@ module HappySeed
 
         generate 'model identity user:references provider:string accesstoken:string uid:string name:string email:string nickname:string image:string phone:string urls:string'
         remove_file 'app/models/identity.rb'
+        remove_file 'spec/models/identity_spec.rb'
         directory 'app'
         directory 'spec'
         route "match '/profile/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup"
