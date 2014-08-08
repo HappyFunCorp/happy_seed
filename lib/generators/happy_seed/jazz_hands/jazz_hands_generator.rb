c386751a7bc2141762a0280a168dc98de3e1f394
Dinesh Vasudevan <dvasudevan@babbel.com>
2014-08-08 23:33:07 +0200

    Jazz hands for Ruby > 2.0 Fix initializers Add more docs

--- lib/generators/happy_seed/jazz_hands/jazz_hands_generator.rb
+++ lib/generators/happy_seed/jazz_hands/jazz_hands_generator.rb
@@ -5,7 +5,19 @@ module HappySeed
 
       def install_jazz_hands
 
-        gem 'jazz_hands', :groups => [:development, :test]
+        gem 'jazz_hands', :github => 'nixme/jazz_hands', :branch => 'bring-your-own-debugger', :groups => [:development, :test]
+
+        ruby_major_version = RUBY_VERSION.split('.')[0]
+
+        if ruby_major_version == '1'
+          gem 'debugger', :groups => [:development, :test]
+        else
+          gem 'byebug', :groups => [:development, :test]
+        end
+
+        inside 'config/initializers' do
+          copy_file 'jazz_hands.rb'
+        end
 
         Bundler.with_clean_env do
           run "bundle install"
