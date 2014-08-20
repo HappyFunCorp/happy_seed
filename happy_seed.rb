a00346bd90d38c4f09747f9ad94da4820016dcce
Aaron Brocken <aaronbrocken@gmail.com>
2014-08-20 11:48:31 -0400

    Added rake task for changing to erb.

--- happy_seed.rb
+++ happy_seed.rb
@@ -1,5 +1,6 @@
-gem 'haml-rails'
 gsub_file 'Gemfile', /.*sqlite3.*/, ""
+gem 'haml-rails'
+gem "httparty"
 
 gem_group :development, :test do
   gem "sqlite3"
@@ -90,6 +91,45 @@ if yes?( "You would like to install jazz hands?" )
   packages << "jazz_hands"
 end
 
+puts "Adding rake task to convert templates to ERB. If you need to do this, please run:"
+puts "rake html2erb"
+rakefile "haml2erb.rake",
+%q{
+desc "Converts everything to erb"
+task :haml2erb do
+  require 'httparty'
+
+  FileUtils.mkdir_p "lib/templates/erb"
+
+  Dir.glob( "**/*.haml").each do |f|
+    puts "Converting #{f}"
+
+    data = File.read( f )
+
+    resp = HTTParty.post( "http://haml2erb.herokuapp.com/api.html", { query: { haml: data } } )
+
+    puts "FROM--------"
+    puts data
+    puts "TO--------"
+    puts resp.body
+
+    outfile = f.gsub( /\.haml/, ".erb" )
+
+    if outfile =~ /templates\/haml/
+      outfile.gsub!( /templates\/haml/, "templates/erb" )
+      puts "outfile = #{outfile}"
+
+      FileUtils.mkdir_p( Pathname.new( outfile ).dirname )
+    end
+
+    File.open( outfile, "w" ) do |out|
+      out.puts resp.body
+    end
+
+    FileUtils.mv( f, "#{f}.bak" )
+  end
+end
+}
 puts "Setting up git"
 git :init
 git add: "."
