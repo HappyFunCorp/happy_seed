b07003a06d24e566c6973a0633eef8a79bd96158
Will Schenk <wschenk@gmail.com>
2015-07-22 14:17:28 -0400

    Fixed up twitter tokens and client

--- lib/generators/happy_seed/twitter/twitter_generator.rb
+++ lib/generators/happy_seed/twitter/twitter_generator.rb
@@ -3,6 +3,8 @@ require 'generators/happy_seed/happy_seed_generator'
 module HappySeed
   module Generators
     class TwitterGenerator < HappySeedGenerator
+      include Rails::Generators::Migration
+
       source_root File.expand_path('../templates', __FILE__)
 
       def install_twitter
@@ -18,7 +20,24 @@ module HappySeed
         add_omniauth :twitter
 
         directory "docs"
+        insert_into_file "app/models/identity.rb", "\n    identity.secrettoken = auth.credentials.secret", after: "identity.accesstoken = auth.credentials.token"
+        migration_template("add_secret_token_to_identity.rb", "db/migrate/add_secret_token_to_identity.rb" )
+
+        gsub_file( "app/models/user.rb", "Twitter.client( access_token: twitter.accesstoken )",
+    "Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV['TWITTER_APP_ID']
+      config.consumer_secret     = ENV['TWITTER_APP_SECRET']
+      config.access_token        = twitter.accesstoken
+      config.access_token_secret = twitter.secrettoken
+    end")
       end
+
+      protected
+
+      def self.next_migration_number(dir)
+        Time.now.utc.strftime("%Y%m%d%H%M%S")
+      end
+
     end
   end
 end
