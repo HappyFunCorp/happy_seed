cd252b48f93186e39d7be7f1fd3ebdcc948d27a9
Will Schenk <wschenk@gmail.com>
2016-03-14 15:09:18 -0400

    Preview mailer only includes things that were installed

--- lib/generators/happy_seed/devise/templates/spec/mailers/previews/devise_preview.rb
+++ lib/generators/happy_seed/devise/templates/spec/mailers/previews/devise_preview.rb
@@ -1,21 +1,28 @@
 class DevisePreview < ActionMailer::Preview
-  def confirmation_instructions
-    Devise::Mailer.confirmation_instructions(user, "tokentokentoken")
+  if User.devise_modules.include? :confirmable
+    def confirmation_instructions
+      Devise::Mailer.confirmation_instructions(user, "tokentokentoken")
+    end
   end
 
   def reset_password_instructions
     Devise::Mailer.reset_password_instructions(user, "tokentokentoken")
   end
 
-  def unlock_instructions
-    Devise::Mailer.unlock_instructions(user, "tokentokentoken")
+  if User.devise_modules.include? :lockable
+    def unlock_instructions
+      Devise::Mailer.unlock_instructions(user, "tokentokentoken")
+    end
   end
 
-  def invitation_instructions
-    Devise::Mailer.invitation_instructions(user, "tokentokentoken")
+  if User.devise_modules.include? :invitable
+    def invitation_instructions
+      Devise::Mailer.invitation_instructions(user, "tokentokentoken")
+    end
   end
 
-  def user
-    User.first || User.new( email: "will@happyfuncorp.com" )
-  end
+  protected
+    def user
+      User.first || User.new( email: "will@happyfuncorp.com" )
+    end
 end
