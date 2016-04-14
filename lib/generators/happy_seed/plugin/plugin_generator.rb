6fa535dabd8c71cae86fa41890ca7c6f53a50a6c
Will Schenk <wschenk@gmail.com>
2016-04-14 18:27:20 -0400

    Updated engine generator

--- lib/generators/happy_seed/plugin/plugin_generator.rb
+++ lib/generators/happy_seed/plugin/plugin_generator.rb
@@ -14,20 +14,22 @@ module HappySeed
 
         insert_into_file "#{app_name}/#{app_name}.gemspec", File.read( "#{source_root}/gemspec" ), :before => "\nend\n"
 
-        system( "cd #{app_name} && bundle" )
+        Bundler.with_clean_env do
+          system( "cd #{app_name} && bundle" )
 
-        system( "cd #{app_name}/spec/dummy && rails g rspec:install")
+          system( "cd #{app_name}/spec/dummy && rails g rspec:install")
 
-        gsub_file "#{app_name}/spec/dummy/.rspec", "--warnings\n", ""
-        append_to_file "#{app_name}/spec/dummy/.rspec", "--format documentation\n"
+          gsub_file "#{app_name}/spec/dummy/.rspec", "--warnings\n", ""
+          append_to_file "#{app_name}/spec/dummy/.rspec", "--format documentation\n"
 
-        self.destination_root = app_name
-        
-    		remove_file "Rakefile"
-        copy_file "Rakefile"
-        copy_file ".rspec"
-        copy_file ".autotest"
-        directory "spec"
+          self.destination_root = app_name
+          
+      		remove_file "Rakefile"
+          copy_file "Rakefile"
+          copy_file ".rspec"
+          copy_file ".autotest"
+          directory "spec"
+        end
       end
     end
   end
